261e184ad4f5fe6702dce8cc7a095b9cf36e06d5
jaypersanchez <jaypersanchez@gmail.com>
2023-01-08 19:00:27 +0800

    added .env file

--- routes/users.js
+++ routes/users.js
@@ -1,14 +1,15 @@
 var express = require('express');
 var router = express.Router();
+require('dotenv').config();
 var Web3 = require('web3')
 var Tx = require('@ethereumjs/tx').Transaction;
-const web3 = new Web3('http://localhost:8545');
+const web3 = new Web3(process.env.PROVIDER);
 
 
-const Private_Key  =  'e2770ed49b7859a0391b25b57750e264f31d05e09d68a40250d17d4e765b1ea7';
-const from_address  =  '0x7323970fe8d4c0F2DD897FF0CA8dAbF5122c26E5';
-const to_address  =  '0x95953992c361e1B2Fe3fCAAeb97Bb600365b9b15';
-const ethAmount = '1000000000000000';
+const Private_Key  =  process.env.PRIVATE_KEY;
+const from_address  =  process.env.FROM_ADDRESS;
+const to_address  =  process.env.TO_ADDRESS;
+const ethAmount = process.env.ETH_AMOUNT;
 
 
 /* GET users listing. */
@@ -31,7 +32,7 @@ const payForWattage = async() => {
     'value': 1000000000000000000, // 1 ETH
     'gas': 30000,
     'nonce': nonce,
-    //'data': {wattage: 10, volts: 110} // optional data field to send message or execute smart contract
+    'data': ""
    };
    const signedTx = await web3.eth.accounts.signTransaction(transaction, Private_Key);
    web3.eth.sendSignedTransaction(signedTx.rawTransaction, function(error, hash) {
